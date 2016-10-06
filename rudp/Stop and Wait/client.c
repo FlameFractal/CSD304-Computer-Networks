2fb5e4180667b7dfb245dcce46657acecef17f58
FlameFractal <vishg8@gmail.com>
2016-10-06 18:34:29 +0530

    refactored and commented

--- rudp/Stop and Wait/client.c
+++ rudp/Stop and Wait/client.c
@@ -1,16 +1,15 @@
 /*
+	LAB 2 SUBMISSION
 
-LAB 2 SUBMISSION
+	USAGE: ./client IP_ADDR [OUTPUT_FILE_NAME (def: out.mp4)] [PORT def: (6005)]
 
-
-TEAM MEMBERS
---------------------
-Saketh Vallakatla - 1410110352
-Prerna - 1410110306
-Vishal Gauba - 1410110501
-Pranjal Mathur - 1410110296
+	TEAM MEMBERS
+	--------------------
+	Prerna - 1410110306
+	Vishal Gauba - 1410110501
+	Pranjal Mathur - 1410110296
+	Saketh Vallakatla - 1410110352
 */
-
 
 
 #include <stdio.h>
@@ -24,146 +23,150 @@ Pranjal Mathur - 1410110296
 #include <sys/time.h>
 
 #define SERVER_PORT 6005
-#define BUF_SIZE 40480+1
+#define PACKET_SIZE 40480+1
 
 long curTimeMillis() {
 	struct timeval te; 
-    gettimeofday(&te, NULL); // get current time
-    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // caculate milliseconds
-    // printf("milliseconds: %lld\n", milliseconds);
+    gettimeofday(&te, NULL); /* get current time */
+    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000;  /* caculate milliseconds */
     return milliseconds;
 }
 
 int main(int argc, char * argv[]){
 
+	printf("\nThis is a Reliable UDP based Client. \nUSAGE: ./client IP_ADDR [OUTPUT_FILE_NAME (def: out.mp4)] [PORT def: (6005)]. \nInitialising processes.\n\n");
+
+
 	FILE *fp;
 	struct hostent *hp;
 	struct sockaddr_in sin;
 	char *host;
-	char buf[BUF_SIZE];
-	int s;
+	int port;
+
+	char* fileName = "out.mp4";
+	char packet[PACKET_SIZE];
+	int sock;
 	int len;
-	int recvBytes = 0;
-	char ack='a';
-	char expectation = '\0';
 
-
-	if ((argc==2)||(argc == 3)) {
-		host = argv[1];
-	}
-	else {
-		fprintf(stderr, "usage: client serverIP [download_filename(optional)]\n");
+	/* Extract arguments */
+	if (argc == 1){
+		printf("Wrong usage: IP_ADDR of server necessary.\n\n");
 		exit(1);
 	}
 
-	if(argc == 3) {
-		fp = fopen(argv[2], "w");
+	if (argc>=2)
+	{
+		host = argv[1];
+	}
+
+	if (argc>=3)
+	{
+		fileName = argv[2];
+		fp = fopen(fileName, "w");
 		if (fp == NULL) {
 			fprintf(stderr, "Error opening output file\n");
 			exit(1);
 		}
 	}
 
-	  /* translate host name into peer's IP address */
+	if (argc==4)
+	{
+		port  = atoi(argv[2]);
+	}
+
+	if (argc>4)
+	{
+		printf("Wrong usage: Expected max 3 arguments.\n");
+	}
+
+
+
+	
+	/* translate host name into peer's IP address */
 	hp = gethostbyname(host);
 	if (!hp) {
-		fprintf(stderr, "client: unknown host: %s\n", host);
+		fprintf(stderr, "Client: unknown host: %s\n", host);
 		exit(1);
 	}
 	else
-		printf("Host %s found!\n", argv[1]);
+		printf("Host '%s' found!\n", argv[1]);
 
-	  /* build address data structure */
+	/* build address data structure */
 	memset((char *)&sin, 0, sizeof(sin));
 	sin.sin_family = AF_INET;
 	memcpy((char *)&sin.sin_addr, hp->h_addr, hp->h_length);
 	sin.sin_port = htons(SERVER_PORT);
 
 
-	  /* create socket */
-	if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("client: socket");
+	/* create socket */
+	if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("Client: socket creation error");
 		exit(1);
 	}
 
-	  /* active open */
-	int c = connect(s, (struct sockaddr *)&sin, sizeof(sin));
+	/* active open */
+	int c = connect(sock, (struct sockaddr *)&sin, sizeof(sin));
 	if (c < 0) {
 		perror("client: connect");
-		close(s);
+		close(sock);
 		exit(1);
 	}
 	else{
 		
-		printf("Client connected to %s:%d. c=%d\n", argv[1], SERVER_PORT,c );
+		printf("Client connected to %s:%d.\n\n", argv[1], SERVER_PORT);
 	}
 
 
-	  /* send message to server */  
-	// fgets(buf, sizeof(buf), stdin);
-	//strcpy(buf,"GET\n");
-	fgets(buf, 4, stdin);
-	strtok(buf,"\n");	
-	buf[BUF_SIZE-1] = '\0';
-//	  char* buf1 = "GET";
-	len = strlen(buf) + 1;
-	send(s, buf, len, 0);
-	printf("Waiting...\n");
+	/* send "get" message to server */  
+	fgets(packet, 4, stdin);
+	strtok(packet,"\n");	
+	packet[PACKET_SIZE-1] = '\0';
+	len = strlen(packet) + 1;
+	send(sock, packet, len, 0);
+	
+	printf("Waiting to recieve.\n");
 	socklen_t client_addr_len = sizeof(sin);
-	int counter = 0;
-
 	long before = curTimeMillis();
 
-	//Initialize expectation
-	expectation='0';
+	int packet_counter = 0;
+	char expectation='0';
+	int recvBytes = 0;
+	int duplicate_counter = 0;
 
-	while(len=40481){
+	while(len = PACKET_SIZE){
 
-		len = recvfrom(s, buf, BUF_SIZE, 0,(struct sockaddr *)&sin, &client_addr_len);
+		len = recvfrom(sock, packet, PACKET_SIZE, 0,(struct sockaddr *)&sin, &client_addr_len);
 
-		if (buf[len-1] == expectation){
-			int expectlen = send(s, &expectation, sizeof(expectation), 0);
-			if (expectlen == -1)
-			{
-				printf("sending ack error\n");
-				exit(1);
-			}
-			else{
-				++counter;
-				recvBytes+=len;
-
-				printf("expectation=%c (New)\n", expectation);
-				expectation = (expectation == '0') ? '1': '0';		//Switch expectation
-				
-				if(argc == 3){
-					fp = fopen(argv[2], "ab+");
-					fwrite(buf, 1,len-1, fp);
-					fclose(fp);
-				}
-			}
+		if (packet[len-1] == expectation){
+			send(sock, &expectation, sizeof(expectation), 0); /* send acknowledgement */
+			printf("Recieved new packet. (Sequence = %c)\n", expectation);
+			
+			packet_counter++;
+			recvBytes+=len;
+			expectation = (expectation == '0') ? '1': '0';	/* Switch expectation */
+			
+			fp = fopen(fileName, "ab+");
+			fwrite(packet, 1,len-1, fp);
+			fclose(fp);
 		}
-		else if(buf[len-1] == (expectation == '0') ? '1': '0'){
-			//Duplicate has been recieved
-			expectation = (expectation == '0') ? '1': '0';		//Send previous expection's acknowledgement
-			int expectlen = send(s, &expectation, sizeof(expectation), 0);
-			printf("last expectation=%c (Duplicate)\n", expectation);
-			expectation = (expectation == '0') ? '1': '0';		//Switch back expectation
+		else if(packet[len-1] == (expectation == '0') ? '1': '0'){	/* Duplicate packet has been recieved */
+			expectation = (expectation == '0') ? '1': '0';		/* Send previous packet's acknowledgement */
+			send(sock, &expectation, sizeof(expectation), 0);
+			printf("Received duplicate packet. (Sequence = %c)\n", expectation);
+			expectation = (expectation == '0') ? '1': '0';		/* Switch back expectation to current*/
+			duplicate_counter++;
 		}
 		else{
 			printf("dont know wtf going on\n");
 		}
 
-		
-		if(strstr(buf, "BYE")) break;
+		if(strstr(packet, "BYE")) break;
 		
 	}
 
 	long after = curTimeMillis();
-
 	long timeTakenMs = (after - before);
-
-	printf("\n");
-	printf("\nRecvBytes = %d Received Packets : %d\n\n", recvBytes, counter);
-	printf("Time Taken(msec) = %ld ms\n\n", (after - before));
-	//printf("Time Taken(sec) = %ld s\n", (after - before)/1000);
+	
+	printf("\n\n\nReceived Bytes = %d, \nReceived Packets (unique) =  %d, \nDuplicate Packets = %d\n", recvBytes, packet_counter, duplicate_counter);
+	printf("Time Taken = %ld ms\n\n", (after - before));
 }
--- rudp/Stop and Wait/server.c
+++ rudp/Stop and Wait/server.c
@@ -1,7 +1,16 @@
-/* CSD 304 Computer Networks, Fall 2016
-   Lab 2, server
-   Team: 
+/*
+	LAB 2 SUBMISSION
+
+	USAGE: ./server FILENAME [TIME_OUT (def: 30000 us)] [IP_ADDR (def: localhost)] [PORT (def: 6005)]
+
+	TEAM MEMBERS
+	--------------------
+	Prerna - 1410110306
+	Vishal Gauba - 1410110501
+	Pranjal Mathur - 1410110296
+	Saketh Vallakatla - 1410110352
 */
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,178 +23,207 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define SERVER_PORT_TEXT 5432
-#define SERVER_PORT_VID 6005
-#define BUF_SIZE_TEXT 4096
-#define BUF_SIZE_VID 40480+1
-
+#define SERVER_PORT 6005
+#define PACKET_SIZE 40480+1
 
 int main(int argc, char * argv[]){
-  struct sockaddr_in sin;
-  struct sockaddr_storage client_addr;
-  char clientIP[INET_ADDRSTRLEN]; /* For IPv4 addresses */
-  socklen_t client_addr_len;
-  char buf[BUF_SIZE_VID];
-  int len;
-  int s;
-  char *host;
-  int sentBytes = 0;
-  struct hostent *hp;
-  char ack = '\0';
-  char expectation = '\0';
 
-  FILE* fp;  
-  char* fileName = "in.mp4";
-//  struct timespec ts;
-  // ts.tv_sec = 0;
-  // // ts.tv_nsec = 200000000L;
-  // ts.tv_nsec = 2000L;
+	printf("\nThis is a Reliable UDP based Server. \nUSAGE: ./server FILENAME [TIME_OUT (def: 30000 us)] [IP_ADDR (def: localhost)] [PORT (def: 6005)]. \nInitialising processes.\n\n");
 
+
+	/* Variable declarations */
+	struct sockaddr_in sin;
+	struct sockaddr_storage client_addr;
+	char clientIP[INET_ADDRSTRLEN]; /* For IPv4 addresses */
+	char *host;
+	socklen_t client_addr_len;
+	struct hostent *hp;	
+	int port;
+
+	char packet[PACKET_SIZE];
+	int len;
+	int sock;
+	int timeout = 30000;
+
+	FILE* fp;  
+	char* fileName = "in.mp4";
+
+
+
+
+	/* Extracting arguments */
+	if (argc == 1)
+	{
+		printf("Wrong usage: Please enter file name to serve.\n\n");
+		exit(1);
+	}
+
+	if (argc >= 2) {
+		fileName = argv[1];  
+		fp = fopen(fileName, "rb");
+		if(fp == NULL){
+			printf("Input file %s: not found",fileName);
+			exit(1);
+		}
+	}
+
+	if (argc >= 3)
+	{
+		timeout = atoi (argv[2]);
+	}
+
+	if (argc >= 4)
+	{
+		host = argv[3];
+	}
+
+	if (argc == 5)
+	{
+		port = atoi (argv[4]);
+	}
+
+	if (argc > 5)
+	{
+		printf("Wrong usage: Expected  max 5 arguments.\n");
+		exit(1);
+	}
+
+
+
+
+	/* Create a socket */
+	if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("server: socket creation error");
+		exit(1);
+	}
+
+	/* build address data structure and bind to all local addresses*/
+	memset((char *)&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+
+	/* If socket IP address specified, bind to it. */
+	if(argc >= 4) {
+		hp = gethostbyname(host);
+		if (!hp) {
+		  fprintf(stderr, "Server: unknown host. Binding to default.\n");
+		  exit(1);
+		}
+		memcpy((char *)&sin.sin_addr, hp->h_addr, hp->h_length);
+	}
+	else /* Else bind to 0.0.0.0 */{
+		sin.sin_addr.s_addr = INADDR_ANY;
+	}
+	
+	sin.sin_port = htons(port);
+  	if (!sin.sin_port)
+  	{
+  		fprintf(stderr, "Server: unknown port. Binding to default.\n");
+  		sin.sin_port = htons(SERVER_PORT);
+  	}
   
+	if ((bind(sock, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+		perror("server: bind");
+		exit(1);
+	}
+	else{ 	/* Add code to parse IPv6 addresses */
+		inet_ntop(AF_INET, &(sin.sin_addr), clientIP, INET_ADDRSTRLEN);
+	}
   
-  if (argc > 2) {
-    fileName = argv[2];  
-  }
-  
-  fp = fopen(fileName, "rb");
-  if(fp == NULL){
-    printf("file %s: not found",fileName);
-    exit(1);
-  }
+	client_addr_len = sizeof(client_addr);
+  	
+  	int c=0;
+  	printf("\n\nServer is listening at address %s:%d\n", clientIP, SERVER_PORT);
+	printf("Client needs to send \"get\" to receive the file : '%s'\nTimeout = %dus\n\n", argv[1], timeout);
+
+	/* Receive messages from clients*/
+	while(len = recvfrom(sock, packet, sizeof(packet), 0,(struct sockaddr *)&client_addr, &client_addr_len)){
+
+		if(c!=0){ /* Print for second onwards clients*/
+			printf("\n\nServer is listening at address %s:%d\n", clientIP, SERVER_PORT);
+			printf("Client needs to send \"get\" to receive the file : '%s'\nTimeout = %dus\n\n", argv[1], timeout);
+		}
+		c=1;
+		inet_ntop(client_addr.ss_family,&(((struct sockaddr_in *)&client_addr)->sin_addr),clientIP, INET_ADDRSTRLEN);
+		rewind(fp);
+		if(strcmp(packet, "get") == 0){
+			
+			printf("Server got message from %s: '%s' [%d bytes]\n\n", clientIP, packet, len);
+		 	
+		 	int size = PACKET_SIZE;
+		 	int packet_counter = 0;
+		 	char expectation = '0';
+			int retransmit_counter = 0;
+			int sentBytes = 0;
+			char ack = '\0';
+
+	 		while(size >= PACKET_SIZE){
+
+				size = fread (packet, 1, sizeof(packet)-1, fp);
+				packet[size] = expectation;  /* Append expectation at the last byte */
+				size++; /* Increment packet size by 1 for sequence number appended to end */
+
+				len = sendto(sock, packet, size, 0,(struct sockaddr *)&client_addr,client_addr_len);
+				printf("Packet %d [size=%d] sending.\n", packet_counter++, size);
+
+				if (len == -1) {
+				  perror("server: sendto");
+				  exit(1);
+				}
+				if(len != size){
+				  printf("ERROR!! Couldn't send all the bytes that were read from the file.");
+				  exit(0);
+				}   
+
+				/* Set recieve timeout in socket options for Stop and Wait timeout */
+				struct timeval timeout_t;
+				timeout_t.tv_sec = 0;
+				timeout_t.tv_usec = timeout;
+				if (setsockopt(sock,SOL_SOCKET, SO_RCVTIMEO, &timeout_t, sizeof(timeout_t)) < 0){
+					perror("server: socket option - set timeout error");
+				}
+
+				while(1){
+
+					recvfrom(sock, &ack, sizeof(ack), 0,(struct sockaddr *)&client_addr, &client_addr_len); /* Try to recieve ack within a set timeout period */
+					printf("Ack = '%c',  ",ack);
+
+					if(ack =='\0'){
+						printf("Timed out. Retransmitting packet  %d.\n",packet_counter);
+						len = sendto(sock, packet, size, 0,(struct sockaddr *)&client_addr,client_addr_len);
+						retransmit_counter++;
+					}
+					else if (ack == expectation){
+						printf("Packet %d acknowledged! Sending next.\n", packet_counter);
+						memset(packet, 0, sizeof(packet));
+						sentBytes+=size;
+						/* Reset ack and switch expectation (for next packet) */
+			  			ack = '\0'; 
+			  			expectation = (expectation == '0') ? '1': '0';
+						break;
+					}
+					else{ /* ack == previous ack (or junk) */
+					  printf("Recieved duplicate ack (of previous packet). Ignoring.\n");
+					  ack='\0';
+					}
+			  	} 
+			}
 
 
-  /* Create a socket */
-  if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-    perror("server: socket");
-    exit(1);
-  }
+			/*	End of transmission.
+				Sending BYE 		*/
+			strcpy(packet, "BYE");
+			sendto(sock, packet, strlen(packet)+1, 0, (struct sockaddr*)&client_addr, client_addr_len);
+			memset(packet, '\0', sizeof(packet));
 
-
-  /* build address data structure and bind to all local addresses*/
-  memset((char *)&sin, 0, sizeof(sin));
-  sin.sin_family = AF_INET;
-
-  /* If socket IP address specified, bind to it. */
-  if(argc == 2) {
-    host = argv[1];
-    hp = gethostbyname(host);
-    if (!hp) {
-      fprintf(stderr, "server: unknown host %s\n", host);
-      exit(1);
-    }
-    memcpy((char *)&sin.sin_addr, hp->h_addr, hp->h_length);
-  }
-  /* Else bind to 0.0.0.0 */
-  else
-    sin.sin_addr.s_addr = INADDR_ANY;
-  
-  sin.sin_port = htons(SERVER_PORT_VID);
-  
-  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
-    perror("server: bind");
-    exit(1);
-  }
-  else{
-    /* Add code to parse IPv6 addresses */
-    inet_ntop(AF_INET, &(sin.sin_addr), clientIP, INET_ADDRSTRLEN);
-    printf("Server is listening at address %s:%d\n", clientIP, SERVER_PORT_VID);
-  }
-  
-  printf("Client needs to send \"GET\" to receive the file %s\n", argv[1]);  
-  
-  client_addr_len = sizeof(client_addr);
-
-
-  
-  /* Receive messages from clients*/
-  while(len = recvfrom(s, buf, sizeof(buf), 0,(struct sockaddr *)&client_addr, &client_addr_len)){
-
-    // len = recvfrom(s, buf, sizeof(buf), 0,(struct sockaddr *)&client_addr, &client_addr_len);
-
-    inet_ntop(client_addr.ss_family,&(((struct sockaddr_in *)&client_addr)->sin_addr),clientIP, INET_ADDRSTRLEN);
-
-    rewind(fp);
-
-    if(strcmp(buf, "get") == 0){
-    	printf("\n\nServer got message from %s: %s [%d bytes]\n", clientIP, buf, len);
-    
-      int size = BUF_SIZE_VID;
-      int counter = 0;
-      expectation = '0';
-
-int retransmitting=0;
-
-      while(!(size < BUF_SIZE_VID)){
-
-        size = fread(buf, 1, sizeof(buf)-1, fp);
-        buf[size] = expectation;  //Append expectation
-        size++; //Increment packet size by 1 for sequence number appended to end
-
-        len = sendto(s, buf, size, 0,(struct sockaddr *)&client_addr,client_addr_len);
-
-        if (len == -1) {
-          perror("server: sendto");
-          exit(1);
-        }
-        if(len != size){
-          printf("ERROR!!");
-          exit(0);
-        }   
-
-      /* Set recieve timeout in socket options */
-        struct timeval timeout;
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 1;
-        if (setsockopt(s,SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0){
-         perror("server: socket option timeout error");
-       }
-
-       while(1){
-
-        int acklen = recvfrom(s, &ack, sizeof(ack), 0,(struct sockaddr *)&client_addr, &client_addr_len);
-        printf("ack = %c. ",ack);
-
-        if(ack =='\0'){
-          printf("Timed out. Retransmitting packet counter %d \n",counter);
-          len = sendto(s, buf, size, 0,(struct sockaddr *)&client_addr,client_addr_len);
-          retransmitting++;
-        }
-        else if (ack == expectation){
-          printf("Packet acknowledged. Sending next.\n");
-          expectation = (expectation == '0') ? '1': '0';  //Switch expectation
-          break;
-        }
-        else{ //ack !=expectation i.e. ack = 1-exp
-          printf("Recieved duplicate ack of previous packet. Ignoring.\n"); //if opposite of expectation is recieved in ack
-          ack='\0';
-        }
-
-      }
-      
-      //Reset ack
-      ack = '\0'; 
-
-      printf("Counter : %d , Size = %d , Len = %d\n", counter++, size, len);
-      memset(buf, 0, sizeof(buf));
-      sentBytes+=size;
-    }
-
-    //SENDING BYE
-    strcpy(buf, "BYE");
-    sendto(s, buf, strlen(buf)+1, 0, (struct sockaddr*)&client_addr, client_addr_len);
-    memset(buf, '\0', sizeof(buf));
-
-    printf("\nSentBytes = %d\n", sentBytes);
-    printf("Retransmitting %d\n",retransmitting );
-    sentBytes=0;
-    //Reset socket option of timeout
-    struct timeval timeout;
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 0;
-    if (setsockopt(s,SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0){
-     perror("server: socket option timeout error");
-   }
-
- }
-} 
+			printf("\n\n\nSent Bytes = %d,\nSent Packets (unique) = %d, \nRetransmitted Packets = %d, \nPercentage of Loss = %d%% \n", sentBytes, packet_counter, retransmit_counter, (retransmit_counter*100)/(retransmit_counter+packet_counter));
+			
+			/* Reset socket option of timeout to recieve request from next client*/
+			struct timeval timeout_t;
+			timeout_t.tv_sec = 0;
+			timeout_t.tv_usec = 0;
+			if (setsockopt(sock,SOL_SOCKET, SO_RCVTIMEO, &timeout_t, sizeof(timeout_t)) < 0){
+				perror("server: socket option timeout error");
+		   }
+		}
+	} 
 } 
