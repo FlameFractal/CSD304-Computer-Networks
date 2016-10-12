0461f524859394e2f79ac4ac17ec61e53dbf9863
FlameFractal <vishg8@gmail.com>
2016-10-12 17:15:06 +0530

    updated rudp/Sliding Window from Prerna's emailed code

--- rudp/Sliding Window/client.c
+++ rudp/Sliding Window/client.c
@@ -1,6 +1,6 @@
 /* CSD 304 Computer Networks, Fall 2016
-   Lab 2, client
-   Team: 
+ Lab 2, client
+ Team: 
 */
 
 #include <stdio.h>
@@ -10,64 +10,191 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include "structures.h"
 
 #define SERVER_PORT 5432
 #define BUF_SIZE 256
-#define RWS 3
+#define RWS 10
 
-int data_received[5];
-int lfr,laf;
+int lfr,laf,incoming_size;
+FILE *output_file;
+f_info *infoRecvd;
+f_data *dataRecvd;
+f_nf * fnfRecvd;
+f_req fileReq;
+ack ackSending;
 
-   int main(int argc, char * argv[]){
+f_req init_f_req(f_req a,int b,char * c)
+{
+	a.type=0;
+	a.filename_size=b;
+	strcpy(a.filename,c);
+	return a;
+}
 
-   	struct hostent *hp;
-   	struct sockaddr_in sin;
-   	char *host;
-   	int s;
-   	int len;
+ack init_ack(ack a,int b)
+{
+	a.type=1;
+	a.num_sequences=1;
+	a.sequence_no[0]=b;
+	return a;
+}
 
-   	if ((argc==2)||(argc == 3)) {
-   		host = argv[1];
-   	}
-   	else {
-   		fprintf(stderr, "usage: client serverIP [download_filename(optional)]\n");
-   		exit(1);
-   	}
+int type_find(void * buf)
+{
+	f_info * var;
+	var=buf;
+	printf("%d\n",var->type);
+	return var->type;
+}
 
 
-  /* translate host name into peer's IP address */
-   	hp = gethostbyname(host);
-   	if (!hp) {
-   		fprintf(stderr, "client: unknown host: %s\n", host);
-   		exit(1);
-   	}
-   	else
-   		printf("Host %s found!\n", argv[1]);
+int main(int argc, char * argv[]){
 
-  /* build address data structure */
-   	memset((char *)&sin, 0, sizeof(sin));
-   	sin.sin_family = AF_INET;
-   	memcpy((char *)&sin.sin_addr, hp->h_addr, hp->h_length);
-   	sin.sin_port = htons(SERVER_PORT);
-   	socklen_t client_addr_len = sizeof(sin);
+	struct hostent *hp;
+	struct sockaddr_in sin;
+	char *host;
+	int s;
+	int len;
+	char blank[1000];
+	void * temp;
+	temp=(void *)(&blank);
+	int length;
+	int index=-1;
+	int type_recvd;
+	int num_recvd;
+	int written=0;
+	int content_write=0;
+	int * ack_counter;
+	char * file_name_input="in.txt";
+	int i;
 
-  /* create socket */
-   	if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-   		perror("client: socket");
-   		exit(1);
-   	}
 
-   	printf("Client will get data from to %s:%d.\n", argv[1], SERVER_PORT);
+	if ((argc==2)||(argc == 3)) {
+		host = argv[1];
+	}
+	else {
+		fprintf(stderr, "usage: client serverIP [download_filename(optional)]\n");
+		exit(1);
+	}
 
-int num;
-     int index=-1;
-  int temp;
-  /* send message to server */  
-   	while(laf-lfr<=RWS)
-   	{
-      len = recvfrom(s,temp,sizeof(int), 0,(struct sockaddr *)&sin, &client_addr_len);
-      data_received[temp]=temp;
-      num = sendto(s,temp, sizeof(int), 0, (struct sockaddr *)&sin, sizeof(sin)); 
-      printf("Received %d and sending ack for %d\n",temp,temp );       
-  	}
+
+/* translate host name into peer's IP address */
+	hp = gethostbyname(host);
+	if (!hp) {
+		fprintf(stderr, "client: unknown host: %s\n", host);
+		exit(1);
+	}
+	else
+		printf("Host %s found!\n", argv[1]);
+
+/* build address data structure */
+	memset((char *)&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	memcpy((char *)&sin.sin_addr, hp->h_addr, hp->h_length);
+	sin.sin_port = htons(SERVER_PORT);
+	socklen_t client_addr_len = sizeof(sin);
+
+/* create socket */
+	if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("client: socket");
+		exit(1);
+	}
+
+	printf("Client will get data from to %s:%d.\n", argv[1], SERVER_PORT);
+	char buf[BUF_SIZE];
+	fgets(buf,BUF_SIZE,stdin);
+	strtok(buf,"\n");
+	fileReq=init_f_req(fileReq,strlen(file_name_input),file_name_input);
+ 	//Send GET 
+	sendto(s,buf,strlen(buf)+1, 0, (struct sockaddr *)&sin, sizeof(sin)); 
+ 	//send File Request
+	sendto(s,&fileReq,sizeof(f_req), 0, (struct sockaddr *)&sin, sizeof(sin)); 
+	recvfrom(s,temp,sizeof(blank), 0,(struct sockaddr *)&sin, &client_addr_len);
+	type_recvd=type_find(temp);
+	if(type_recvd==4)
+	{
+		printf("No such file found\n");
+	}
+	else
+	{
+		output_file=fopen("out.txt","wb");
+		infoRecvd=temp;
+		if((infoRecvd->file_size%infoRecvd->block_size)==0)
+		{
+			incoming_size=((infoRecvd->file_size)/(infoRecvd->block_size))-1;
+		}
+		else
+		{
+			incoming_size=((infoRecvd->file_size)/(infoRecvd->block_size));
+		}
+		fwrite(&infoRecvd->data,1,infoRecvd->block_size,output_file);
+ 		//set up ack_counter
+		ack_counter=malloc(sizeof(int)*(incoming_size+1));
+		for(i=0;i<=incoming_size;i++)
+		{
+			ack_counter[i]=0;
+		}
+ 		//initialize data buffer
+//		printf("malloced data buffer and memseted with sizeof data_buffer as %d while it should be %d\n",sizeof(*data_buffer),(sizeof(char)*infoRecvd->block_size)*RWS);
+		char data_buffer[infoRecvd->block_size*RWS];
+		memset(data_buffer,'\0',sizeof(data_buffer));
+		laf=RWS;
+		while(lfr<incoming_size)
+		{
+			length = recvfrom(s,temp,sizeof(blank), 0,(struct sockaddr *)&sin, &client_addr_len);
+			type_recvd=type_find(temp);
+			if (type_recvd==3)
+			{
+				dataRecvd=temp;
+				num_recvd=dataRecvd->sequence_number;
+
+				printf("Received %d and laf is %d \n",num_recvd,laf);
+				if((length>0)&&(num_recvd<=laf))
+				{
+					{
+						ackSending=init_ack(ackSending,num_recvd);
+						length = sendto(s,&ackSending, sizeof(ack), 0, (struct sockaddr *)&sin, sizeof(sin)); 
+						memcpy(&data_buffer[(num_recvd*infoRecvd->block_size)%RWS],&dataRecvd->data,infoRecvd->block_size);
+						printf("Sending ack for %d\n",num_recvd );
+						ack_counter[num_recvd]=1;
+						for(i=written;i<=num_recvd;i++)
+						{
+							if(ack_counter[i+1]==1)
+							{
+								lfr=i+1;
+								laf++;
+							}
+							else
+							{
+								break;
+							}
+						}
+
+						if(lfr!=written)
+						{
+							for (i = written+1; i <= lfr;i++)
+							{
+								if(fwrite(&data_buffer[(i*(infoRecvd->block_size)%RWS)],1,dataRecvd->block_size,output_file)<0){
+									printf("fuckeeeeeeeeeeeeeeed\n");
+								}
+							}
+						}
+						written=lfr; 
+					}  
+				}
+				printf("lfr=%d\n",lfr );
+			}
+
+			else
+			{
+				printf("Received something other than data block\n");
+				exit(0);
+			}
+
+		}
+		fclose(output_file);
+		exit(0);
+}
 }
--- rudp/Sliding Window/server.c
+++ rudp/Sliding Window/server.c
@@ -1,149 +1,363 @@
-/* CSD 304 Computer Networks, Fall 2016
-   Lab 2, server
-   Team: 
+     #include <stdio.h>
+     #include<pthread.h>
+     #include <stdlib.h>
+     #include <string.h>
+     #include <sys/types.h>
+     #include <sys/socket.h>
+     #include<time.h>
+     #include <netinet/in.h>
+     #include <netdb.h>
+     #include <arpa/inet.h>
+     #include "structures.h"
+
+     #define SERVER_PORT 5432
+     #define BUF_SIZE 256
+     #define SWS 10
+     #define TIMEOUT 0.1
+  //   #define SEND_SIZE 10000
+     /*FUNCTION PROTOTYPES*/
+int check_timeout(clock_t start,int num);
+void * receive_acks();
+
+    /*Function Definitions*/
+int type_find(void * buf)
+{
+  f_req * var;
+  var=buf;
+  printf("%d\n",var->type);
+  return var->type;
+}
+
+int getSize(FILE * fp)
+{
+  FILE * tem=fp;
+  int size;
+  fseek(tem,0L,SEEK_END);
+  size=ftell(tem);
+  rewind(fp);
+  return size;
+}
+
+f_nf init_f_nf(f_nf a,int b,char * c)
+{
+  a.type=4;
+  a.filename_size=b;
+  strcpy(a.filename,c);
+  return a;
+}
+
+f_data init_f_data(f_data a,int b,int c,char * d)
+{
+  a.type=3;
+  a.sequence_number=b;
+  a.block_size=c;
+  strcpy(a.data,d);
+  return a;
+}
+
+f_info init_f_info(f_info a,int b,int c,char * d,int e,int f,char * g)
+{
+  a.type=2;
+  a.sequence_number=b;
+  a.filename_size=c;
+  strcpy(a.filename,d);
+  a.file_size=e;
+  a.block_size=f;
+  strcpy(a.data,g);
+  return a;
+}
+
+     /*GLOBAL VARIABLES*/
+int lar,lfs,sendSize,fileSize;
+int no_of_blocks;
+clock_t * start_vals;
+int * ack_recv;
+FILE * fp;
+char buf[BUF_SIZE];
+f_req * fileReqstd;
+ack * ackRecvd;
+f_nf ReqNF;
+f_info fileInfo;
+f_data fileData;
+     /*variables for UDP setup*/
+int udpSocket;
+
+struct sockaddr_in serverAddr,clientAddr;
+
+struct sockaddr_storage serverStorage;
+socklen_t addr_size,client_addr_size;
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char const *argv[])
+{
+  char blank[1000];
+  void * temp;
+  temp=(void *)(&blank);
+  int len=0;
+  int index,i;
+  pthread_t ack_thread;
+  lar=0;
+  lfs=0;
+//  ack_recv[0]=1;
+
+        /*create socket*/
+  if ((udpSocket = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("server: socket");
+    exit(1);
+  }
+
+     /*configuration of server address*/
+  serverAddr.sin_family=AF_INET;
+  serverAddr.sin_port=htons(5432);
+  serverAddr.sin_addr.s_addr=INADDR_ANY;
+
+     /*binding socket to address structure*/
+  bind(udpSocket,(struct sockaddr *)&serverAddr,sizeof(serverAddr));
+
+     /*initialize client address size*/
+  addr_size=sizeof(serverStorage);
+  printf("Client needs to send \"GET\" to receive the file %s\n", argv[1]);
+  //Receive GET message
+  recvfrom(udpSocket,buf,BUF_SIZE, 0,(struct sockaddr *)&serverStorage, &addr_size);
+  
+  /*
+  sendto(udpSocket,&sendSize, sizeof(int), 0,(struct sockaddr *)&serverStorage, addr_size);
+  int i;
+  for (i = 1; i <=SEND_SIZE; i++)
+  {
+    data[i]=i;
+  }
 */
+  while(strcmp(buf,"GET")!=0)
+  {}
 
-#include <stdio.h>
-#include<pthread.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include<time.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
+printf("out of while loop\n");
 
-#define SERVER_PORT 5432
-#define BUF_SIZE 256
-#define SWS 3
-   #define TIMEOUT 5
+  //Receive filename request
+recvfrom(udpSocket,temp,sizeof(blank), 0,(struct sockaddr *)&serverStorage, &addr_size);
+int type=type_find(temp);
 
-   int check_timeout(clock_t start);
-   void * receive_acks();
+index=1;
 
-
-   int lar,lfs;
-   clock_t start_vals[5];
-   int ack_recv[5];
-   struct sockaddr_in sin_variable;
-   struct sockaddr_storage client_addr;
-   char clientIP[INET_ADDRSTRLEN]; /* For IPv4 addresses */
-   socklen_t client_addr_len;
-   char *host;
-   struct hostent *hp;
-   int s;
-   
-
-   int main(int argc, char * argv[]){
-    pthread_t ack_thread;
-    lar=-1;
-    lfs=-1;
-    int index;
-
-  /* Create a socket */
-     if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-      perror("server: socket");
-      exit(1);
-    }
-
-
-  /* build address data structure and bind to all local addresses*/
-    memset((char *)&sin_variable, 0, sizeof(sin_variable));
-    sin_variable.sin_family = AF_INET;
-
-  /* If socket IP address specified, bind to it. */
-    if(argc == 2) {
-      host = argv[1];
-      hp = gethostbyname(host);
-      if (!hp) {
-        fprintf(stderr, "server: unknown host %s\n", host);
-        exit(1);
-      }
-      memcpy((char *)&sin_variable.sin_addr, hp->h_addr, hp->h_length);
-    }
-  /* Else bind to 0.0.0.0 */
-    else
-      sin_variable.sin_addr.s_addr = INADDR_ANY;
-
-    sin_variable.sin_port = htons(SERVER_PORT);
-
-    if ((bind(s, (struct sockaddr *)&sin_variable, sizeof(sin_variable))) < 0) {
-      perror("server: bind");
-      exit(1);
-    }
-    else{
-    /* Add code to parse IPv6 addresses */
-      inet_ntop(AF_INET, &(sin_variable.sin_addr), clientIP, INET_ADDRSTRLEN);
-      printf("Server is listening at address %s:%d\n", clientIP, SERVER_PORT);
-    }
-
-    printf("Client needs to send \"GET\" to receive the file %s\n", argv[1]);
-    int i=0;
-    for(i=0;i<5;i++)
-    {
-      ack_recv[i]=0;
-    }
-
-    int data[]={1,2,3,4,5};
-    index=-1;
-    int len=0;
-    pthread_create(&ack_thread,NULL,receive_acks,NULL);
+if(type==0)
+{
+  fileReqstd=temp;
+  fp=fopen(fileReqstd->filename,"rb");
+  if(fp==NULL)
+  {
+    ReqNF=init_f_nf(ReqNF,fileReqstd->filename_size,fileReqstd->filename);
+    sendto(udpSocket,&(ReqNF), sizeof(f_nf), 0,(struct sockaddr *)&serverStorage, addr_size);
+  }
+  else
+  {
+      //create ack_thread
+    //pthread_create(&ack_thread,NULL,receive_acks,NULL);
     printf("Created new thread\n");
-    while(index<5)
+    fileSize=getSize(fp);
+    if((fileSize%BUF_SIZE)==0)
     {
-      while(lfs-lar<=SWS)
-      {
-        lfs++;
-        len = sendto(s,&data[index++], sizeof(int), 0,(struct sockaddr *)&client_addr, client_addr_len);
-        start_vals[index]=clock();       
-      }
-      //will comeout of loop if timeout of ack or lost
-      {
-        len = sendto(s,&data[lar+1], sizeof(int), 0,(struct sockaddr *)&client_addr, client_addr_len);
-        start_vals[lar+1]=clock();
-      }
-      
-    }
-  }
-
-  void * receive_acks()
-  {
-    int length,i;
-    int ack_num;
-    length = recvfrom(s,&ack_num,sizeof(int), 0,(struct sockaddr *)&client_addr, &client_addr_len);
-    if(check_timeout(start_vals[ack_num])==0)
-    {
-      ack_recv[ack_num]=1;
-      printf("Adding ack for %d\n",ack_num);
-      for(i=lar;i<ack_num;i++)
-      {
-        if(ack_recv[i+1]==1)
-        {
-          lar=i;
-        }
-        else
-        {
-          break;
-        }
-      }
-      printf("The LAR is %d\n",lar );
-    }
-    else{
-      printf("ACK no. %d received after timeout\n",ack_num );
-    }
-    fflush(stdout);
-  }
-
-  int check_timeout(clock_t start)
-  {
-    clock_t curr=clock();
-    if((curr-start)>=(TIMEOUT*CLOCKS_PER_SEC))
-    {
-      return 1;
+      sendSize=(fileSize/BUF_SIZE)-1;     
     }
     else
     {
-      return 0;
+      sendSize=(fileSize/BUF_SIZE);    
+    }
+      //initializing ack_buffer
+    ack_recv=calloc(sendSize+1,sizeof(int));
+   //  for(i=1;i<=sendSize;i++)
+   //  {
+   //   ack_recv[i]=0;
+   // }
+
+  //initialize time buffers
+   start_vals=malloc(sizeof(clock_t)*(sendSize+1));
+
+    //initialize data buffer
+   char data[BUF_SIZE*sendSize];
+   int count_read;
+   printf("gonna read that shit now\n");
+   if(!(count_read=fread(buf,1,BUF_SIZE,fp))){
+    printf("lolllllllllllzzzzzzzzzzzzz\n");
+   }
+   printf("%d\n",count_read );
+   fprintf(stdout, "%s\n",buf );
+   fileInfo=init_f_info(fileInfo,index,fileReqstd->filename_size,fileReqstd->filename,fileSize,BUF_SIZE,buf);
+   len=sendto(udpSocket,&(fileInfo), sizeof(f_info), 0,(struct sockaddr *)&serverStorage, addr_size);
+  if(!(count_read=fread(buf,1,BUF_SIZE,fp))){
+    printf("lollllllll 2\n");
+   }
+      fprintf(stdout, "%s\n",buf );
+   /*while(count_read==BUF_SIZE)
+   {
+    fileData=init_f_data(fileData,++seq_count,count_read,buf);
+    sendto(udpSocket,&(fileData), sizeof(f_data), 0,(struct sockaddr *)&serverStorage, addr_size);
+    count_read=fread(buf,1,256,fp);
+  }
+  if (count_read>0)
+  {
+    fileData=init_f_data(fileData,++seq_count,count_read,buf);  
+    sendto(udpSocket,&(fileData), sizeof(f_data), 0,(struct sockaddr *)&serverStorage, addr_size);
+  }    
+  */
+  while(index<=sendSize||(lar<sendSize))
+  {
+    while((lfs-lar<SWS)&&(index<=sendSize))
+    {    
+      fileData=init_f_data(fileData,index,count_read,buf);
+      sendto(udpSocket,&(fileData), sizeof(f_data), 0,(struct sockaddr *)&serverStorage, addr_size);
+      start_vals[index]=clock();
+      lfs++;
+      index++;
+      memcpy(&data[(lfs%SWS)*BUF_SIZE],buf,BUF_SIZE);
+      count_read=fread(buf,1,BUF_SIZE,fp);    
+      if(index>sendSize)
+      {
+       break;
+     }
+     if(len==-1)
+     {
+      perror("Sendto error:");
     }
   }
+
+  while((check_timeout(start_vals[lar+1],lar+1)==0)&&(ack_recv[lar+1]==0)&&(lar<lfs)&&(lfs-lar>=SWS)){}
+
+    if((ack_recv[lar+1]==0)&&(lfs-lar>=SWS))
+    {
+     printf("Retransmitting %d\n",(lar+1));
+     fileData=init_f_data(fileData,lar+1,strlen(data[((lar+1)%SWS)*BUF_SIZE]),data[(lar+1)%SWS]);
+     sendto(udpSocket,&(fileData), sizeof(f_data), 0,(struct sockaddr *)&serverStorage, addr_size);
+     start_vals[lar+1]=clock();
+   }
+ }
+/*
+ int count_read=fread(buf,1,256,fp);
+ fileInfo=init_f_info(fileInfo,++seq_count,fileReqstd->filename_size,fileReqstd->filename,getSize(fp),BUF_SIZE,buf);
+ sendto(udpSocket,&(fileInfo), sizeof(f_info), 0,(struct sockaddr *)&serverStorage, addr_size);
+ count_read=fread(buf,1,256,fp);
+ while(count_read==BUF_SIZE)
+ {
+  fileData=init_f_data(fileData,++seq_count,count_read,buf);
+  sendto(udpSocket,&(fileData), sizeof(f_data), 0,(struct sockaddr *)&serverStorage, addr_size);
+  count_read=fread(buf,1,256,fp);
+}
+if (count_read>0)
+{
+  fileData=init_f_data(fileData,++seq_count,count_read,buf);  
+  sendto(udpSocket,&(fileData), sizeof(f_data), 0,(struct sockaddr *)&serverStorage, addr_size);
+}*/
+}
+}
+else
+{
+  printf("No request received and quitting\n");
+  exit(0);  
+}
+
+
+  //fclose(fp);
+/*
+while(index<=SEND_SIZE||(lar<SEND_SIZE))
+{
+ while((lfs-lar<=SWS)&&(index<=SEND_SIZE))
+ {        
+
+  len = sendto(udpSocket,&data[index], sizeof(int), 0,(struct sockaddr *)&serverStorage, addr_size);
+  start_vals[index]=clock();       
+        //          printf("Sending %d \n",data[index]);
+  lfs++;
+  index++;
+  if(index>SEND_SIZE+1)
+  {
+    break;
+  }
+  if(len==-1)
+  {
+   perror("Sendto error:");
+ }
+}
+       //   printf("lfs-lar is greater than SWS with lfs=%d and lar=%d\n",lfs,lar);
+                         //will comeout of loop if timeout or lost
+
+while((check_timeout(start_vals[lar+1],lar+1)==0)&&(ack_recv[lar+1]==0)&&(lar<lfs)&&(lfs-lar>SWS)){}
+
+ if((ack_recv[lar+1]==0)&&(lfs-lar>SWS))
+ {
+  printf("Retransmitting %d\n",data[lar+1]);
+  len = sendto(udpSocket,&data[lar+1], sizeof(int), 0,(struct sockaddr *)&serverStorage, addr_size);
+  start_vals[lar+1]=clock();
+}
+
+
+}
+*/
+}
+
+void * receive_acks()
+{
+  printf("Inside new thread\n");
+  fflush(stdout);
+  char blank2[1000];
+  void * temp2;
+  temp2=(void *)(&blank2);
+
+  int length,i,check,type_recv;
+  int ack_num;
+  printf("going into new thread while \n");
+  while(1){
+   length = recvfrom(udpSocket,temp2,sizeof(blank2), 0,(struct sockaddr *)&serverStorage, &addr_size);
+     //printf("Received ack %d\n",ack_num );
+   type_recv=type_find(temp2);
+   if(type_recv==1)
+   {
+      ackRecvd=temp2;
+      ack_num=ackRecvd->sequence_no[0];
+      if(check_timeout(start_vals[ack_num],ack_num)==0)
+      {
+       ack_recv[ack_num]=1;
+       check=1;
+       for(i=lar+1;i<=ack_num;i++)
+       {
+        if(ack_recv[i]==0)
+        {
+         check=0;
+         printf("ACK missing for %d\n",i );
+         lar=i-1;
+         break;
+       }
+     }
+     if(check==1)
+     {
+      lar=ack_num;
+    }
+          // printf("The LAR is %d\n",lar );
+   }
+   else{
+    printf("ACK no. %d received after timeout\n",ack_num );
+   }
+   memset(&ack_num,'\0',sizeof(int));
+fflush(stdout); 
+}
+else{
+  printf("Received from client unexpected or undecodable message\n");
+  exit(0);
+}
+   
+}
+}
+
+
+int check_timeout(clock_t start,int num)
+{
+  clock_t curr=clock();
+    //printf("current time is %d\n",(int)curr );
+  if((curr-start)>=(TIMEOUT*CLOCKS_PER_SEC))
+  {
+//     printf("Timeout occured for %d \n",num);
+   return 1;
+ }
+ else
+ {
+  return 0;
+}
+}
